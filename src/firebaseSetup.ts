18b50091f85260092d75c15241b543d671674d6d
Kafelixon <piotrszkafel@gmail.com>
2023-08-04 14:04:24 +0200

    Note: when using vite, use import.meta not process

--- src/firebaseSetup.ts
+++ src/firebaseSetup.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'mock_key',
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'mock_key',
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'mock_key',
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'mock_key',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || 'mock_key',
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || 'mock_key',
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'mock_key'
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const firebase = initializeApp(firebaseConfig);
