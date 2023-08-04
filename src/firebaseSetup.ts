7fe7516cbaf454a2f934d889c8b339a643b0033d
Kafelixon <piotrszkafel@gmail.com>
2023-08-04 13:52:08 +0200

    Added defaults, should help with firebase issues

--- src/firebaseSetup.ts
+++ src/firebaseSetup.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || 'mock_key',
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || 'mock_key',
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || 'mock_key',
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || 'mock_key',
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || 'mock_key',
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || 'mock_key',
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || 'mock_key'
 };
 
 const firebase = initializeApp(firebaseConfig);
