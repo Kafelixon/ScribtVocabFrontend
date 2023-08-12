5207548b080fead425fd0f2aa252fc8e9b60f113
Kafelixon <piotrszkafel@gmail.com>
2023-08-12 21:04:32 +0200

    Added loading and empty dictionary handling

--- components/PersonalDictionary.tsx
+++ components/PersonalDictionary.tsx
@@ -5,21 +5,48 @@ import { ResponseData } from "../src/types";
 import { auth } from "../src/firebaseSetup";
 
 export const PersonalDictionary: React.FC = () => {
-  const userId = auth.currentUser?.uid;
   const [userDictionary, setUserDictionary] = useState<ResponseData[] | null>(
     null
   );
+  const [userId, setUserId] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserId(user.uid);
+      } else {
+        setUserId(null);
+        setUserDictionary(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   useEffect(() => {
     if (userId) {
+      setIsLoading(true);
       fetchUserDictionary(userId).then((data) => {
         setUserDictionary(data);
+        setIsLoading(false);
       });
     }
   }, [userId]);
+
+  if (isLoading) {
+    return <p>Loading...</p>; // Replace with loading spinner or component
+  }
+
   return (
     <>
-      {userDictionary && (
+      <h1>Personal Dictionary</h1>
+      {userDictionary && userDictionary.length > 0 ? (
         <TranslatedResponseTable response={{ data: userDictionary }} />
+      ) : (
+        <p>Your personal dictionary is empty.</p>
       )}
     </>
   );
